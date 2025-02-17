f49f79353c7546ba13a388680656136d8a866d6e
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-02-17 05:50:11 +0300

    Successfuly rendered templates files on the server

--- Backend/settings.py
+++ Backend/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = (
+    "3_i3O-Gbjwc2UjgF6BOO9zQOFjtqN-aXyqAu4A_rnCGuBcwOC4s51BHvvjBEoguRgHA"
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Travel/urls.py
+++ Travel/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path,include
 from django.contrib import admin
-from . import views 
+from . import views
 
 
 urlpatterns = [
     path("", views.home, name="home.html"),
-    
 ]
